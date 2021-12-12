b8f4d68d3da4209bac107dc3cdcc9aa6d61fcb40
Neetz78 <neethu.g0807@gmail.com>
2021-12-11 19:38:58 -0800

    NonVeg Try catch

--- recipes/NonVeg/nveg.py
+++ recipes/NonVeg/nveg.py
@@ -1,7 +1,25 @@
+class NonVegError(Exception):
+    pass
+class LevelIsNumber(NonVegError):
+    pass
+class LevelIsInvalid(NonVegError):
+    pass
 '''Module for taking user input for non vegetarian recipes'''
 def level():
     '''Returns the level of difficulty selected by the user
     '''
     print("Choose the difficulty level\n")
-    level = input("Easy , Medium , Hard :")
-    return level
+    try:
+        level = input("Easy , Medium , Hard:")
+        
+        if level.isdigit():
+            raise LevelIsNumber()
+        elif level not in ["Easy","easy","e","EASY","eas","Medium","medium","m","MEDIUM","med","Hard","hard","h","HARD","hrd"]:
+            raise LevelIsInvalid()
+        
+    except LevelIsNumber:
+        print("The level entered is a number. Kindly re-enter.")
+    except LevelIsInvalid:
+        print("Please enter a valid preference. Kindly re-enter.")
+    finally:
+        return level
