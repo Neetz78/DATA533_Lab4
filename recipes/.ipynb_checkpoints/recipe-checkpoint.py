3ac2dd797857f1a0ccc46a3b9145f6275e441310
DishaDH123 <dishaholla04@gmail.com>
2021-12-11 19:17:36 -0800

    recipe try catch

--- recipes/.ipynb_checkpoints/recipe-checkpoint.py
+++ recipes/.ipynb_checkpoints/recipe-checkpoint.py
@@ -1,3 +1,9 @@
+class Error(Exception):
+    pass
+class InputIsNumberError(Error):
+    pass
+class InputInvalidError(Error):
+    pass
 def recipe():
     
     '''The recipe package
@@ -19,6 +25,7 @@ def recipe():
     
     import pandas as pd
     import recipes.Veg.veg as veg
+    import string
     import recipes.NonVeg.nveg as nveg
     from recipes.Veg import easy as e,intermediate as i,difficult as d 
     from recipes.NonVeg import easy as ne,intermediate as ni,difficult as nd
@@ -27,37 +34,59 @@ def recipe():
 
     data = pd.read_csv("Recipes.csv",encoding = "ISO-8859-1", engine='python')
 
-    print("Please select your food preference")
-    perf = input("Vegetarian or Non-Vegetarian:")
-    if (perf == "Vegetarian" or perf == "Veg" or perf == "veg" or perf == "vegetarian"):
-        lev = veg.level()
-        if (lev == "Easy" or lev == "easy" or lev == "e" or lev == "EASY"):
-            a=e.steps(data)
-            a.seasy()
-        elif (lev == "Medium" or lev == "medium" or lev == "m" or lev == "MEDIUM"):
-            a=i.steps(data)
-            a.smedium()
-        elif (lev == "Hard" or lev == "hard" or lev == "d" or lev == "HARD"):
-            a=d.steps(data)
-            a.sdiff()
-        else:
-            pass
+    print("Please select your food preference or type exit to exit")
     
-    elif (perf == "Non-Vegetarian" or perf == "Non-Veg" or perf == "nonveg" or perf == "nonvegetarian" or perf == "NonVegetarian" or perf == "Nonveg"):
-        nlev = nveg.level()
-        if (nlev == "Easy" or nlev == "easy" or nlev == "e" or nlev == "EASY"):
-            a=ne.steps(data)
-            a.seasy()
-        elif (nlev == "Medium" or nlev == "medium" or nlev == "m" or nlev == "MEDIUM"):
-            a=ni.steps(data)
-            a.smedium()
-        elif (nlev == "Hard" or nlev == "hard" or nlev == "d" or nlev == "HARD"):
-            a=nd.steps(data)
-            a.sdiff()
+    try:
+        perf = input("Vegetarian or Non-Vegetarian:")
+        
+        if perf.isdigit():
+            raise InputIsNumberError()
+        elif perf not in ["Vegetarian","Veg","veg","vegetarian","Non-Vegetarian","Non-Veg","nonveg","nonvegetarian" ,"NonVegetarian","Nonveg", "exit","Exit"]:
+            raise InputInvalidError()
+        
+    except InputIsNumberError:
+        print("The preference entered is a number. Kindly re-enter or type exit to exit")
+    except InputInvalidError:
+        print("Please enter a valid preference. Kindly re-enter or type exit to exit")
+        
+        
+    else:
+        
+        if (perf == "Vegetarian" or perf == "Veg" or perf == "veg" or perf == "vegetarian"):
+            lev = veg.level()
+            if (lev == "Easy" or lev == "easy" or lev == "e" or lev == "EASY" or lev=="eas"):
+                a=e.steps(data)
+                a.seasy()
+            elif (lev == "Medium" or lev == "medium" or lev == "m" or lev == "MEDIUM" or lev== "med"):
+                a=i.steps(data)
+                a.smedium()
+            elif (lev == "Hard" or lev == "hard" or lev == "d" or lev == "HARD" or lev=="hrd"):
+                a=d.steps(data)
+                a.sdiff()
+            else:
+                pass
+    
+        elif (perf == "Non-Vegetarian" or perf == "Non-Veg" or perf == "nonveg" or perf == "nonvegetarian" or perf == "NonVegetarian" or perf == "Nonveg"):
+            nlev = nveg.level()
+            if (nlev == "Easy" or nlev == "easy" or nlev == "e" or nlev == "EASY"):
+                a=ne.steps(data)
+                a.seasy()
+            elif (nlev == "Medium" or nlev == "medium" or nlev == "m" or nlev == "MEDIUM"):
+                a=ni.steps(data)
+                a.smedium()
+            elif (nlev == "Hard" or nlev == "hard" or nlev == "d" or nlev == "HARD"):
+                a=nd.steps(data)
+                a.sdiff()
+            else:
+                pass
         else:
             pass
-    else:
-        pass
+        
+    finally:
+        if perf != "exit":
+            recipe()
+    
+    
 
 
 
